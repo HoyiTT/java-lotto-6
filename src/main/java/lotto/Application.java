bebf61d679c0aa8c47079819145e2d2ba607f438
HoyiTT <pjs9177@naver.com>
2023-11-07 07:42:52 +0900

    [Feat] 당첨 번호 및 보너스 번호 입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -16,14 +16,14 @@ public class Application {
 
         int payLottoCount = money / 1000;
 
-        int [][] lotooNumbers = new int [payLottoCount][6];
+        int [][] lottoNumbers = new int [payLottoCount][6];
 
         for(int i = 0 ; i < payLottoCount ; i++){
             for(int k = 0 ; k < 6; k++){
-                while (lotooNumbers[i][k] < 6) {
+                while (lottoNumbers[i][k] < 6) {
                     int randomNumber = Randoms.pickNumberInRange(1, 45);
-                    if (!(Arrays.asList(lotooNumbers[i]).contains(randomNumber))) {
-                        lotooNumbers[i][k] = randomNumber;
+                    if (!(Arrays.asList(lottoNumbers[i]).contains(randomNumber))) {
+                        lottoNumbers[i][k] = randomNumber;
                     }
                 }
             }
@@ -32,12 +32,27 @@ public class Application {
         for(int i = 0 ; i < payLottoCount ; i++){
             System.out.print("[");
             for(int k = 0 ; k < 5; k++){
-                System.out.print(lotooNumbers[i][k]);
+                System.out.print(lottoNumbers[i][k]);
                 System.out.print(", ");
             }
-            System.out.print(lotooNumbers[i][5]);
+            System.out.print(lottoNumbers[i][5]);
             System.out.print("]");
             System.out.println();
         }
+
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String userNumber  = Console.readLine();
+        String[] inputArray =userNumber.split(",");
+        int[] userNumbers = new int[6];
+        for (int i = 0; i < inputArray.length; i++) {
+            userNumbers[i] = Integer.parseInt(inputArray[i].trim());
+        }
+
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String bonusNumberInput  = Console.readLine();
+        int bonusNumber = Integer.parseInt(bonusNumberInput);
+
+
+
     }
 }
---
+++ src/main/java/lotto/Input.java
@@ -0,0 +1,6 @@
+package lotto;
+
+public class Input {
+
+    public generate
+}
